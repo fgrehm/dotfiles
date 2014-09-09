cdc70033e7f42c353e04e8ca883e7820d02e0bd0
Fabio Rehm <fgrehm@gmail.com>
2014-09-09 14:54:06 -0300

    Devstep default configs

--- setup.sh
+++ setup.sh
@@ -23,6 +23,15 @@ else
   echo "  SKIP - ${link_name}"
 fi
 
+target="`pwd`/devstep.yml"
+link_name="${HOME}/devstep.yml"
+if ! [[ -L $link_name ]]; then
+  echo "  LINK - ${link_name}"
+  sudo ln -s $target $link_name
+else
+  echo "  SKIP - ${link_name}"
+fi
+
 if ! $(grep -q "source `pwd`/bashrc" $HOME/.bashrc); then
   echo "Adding source to bashrc"
   echo "source `pwd`/bashrc" >> $HOME/.bashrc
